a6469305dfd318c39de369bca1a721f44a24c64a
monish_p <monish_p@LP-PG02JDG6.HCLT.CORP.HCL.IN>
2021-12-29 17:14:30 +0530

    Added cross origin support

--- crs-backend/src/main/java/com/hcl/controllers/BookingController.java
+++ crs-backend/src/main/java/com/hcl/controllers/BookingController.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hcl.model.Booking;
 import com.hcl.services.BookingService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class BookingController {
 
